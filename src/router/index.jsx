8e920adee34e7a43c24a1283b1508a6fc6b47209
marycala <marycala87@gmail.com>
2025-06-20 11:25:06 +0200

    refactor(routing): integrar useNavigate para manejar redirección en API client

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Events from "../pages/Events/Events";
 import EditEvent from "../pages/EditEvent/EditEvent";
@@ -10,24 +10,38 @@ import Attendances from "../pages/MyAttendances/MyAttendances";
 import NotFound from "../pages/NotFound/NotFound";
 import EventDetail from "../pages/EventDetail/EventDetail";
 import AppLayout from "../layouts/AppLayout";
+import { useEffect } from "react";
+import { setApiNavigate } from "../api";
+
+const RouterWithNavigate = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setApiNavigate(navigate);
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/events/edit/:id" element={<EditEvent />} />
+        <Route path="/events/create" element={<CreateEvent />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/attendances" element={<Attendances />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/events/:id" element={<EventDetail />} />
-          <Route path="/events/edit/:id" element={<EditEvent />} />
-          <Route path="/events/create" element={<CreateEvent />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/attendances" element={<Attendances />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <RouterWithNavigate />
     </BrowserRouter>
   );
 };
