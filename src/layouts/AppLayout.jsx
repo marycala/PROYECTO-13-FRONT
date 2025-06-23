411d70bb9ddc3b39b27fd9b4f9cfcb129aa00a60
marycala <marycala87@gmail.com>
2025-06-23 13:21:43 +0200

    aplicar padding solo en páginas distintas a Home para evitar espacio extra debajo del header

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -2,15 +2,24 @@ import { Box } from "@chakra-ui/react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const AppLayout = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <Box minH="100vh" display="flex" flexDirection="column">
       <Box h="9vh">
         <Header />
       </Box>
 
-      <Box id="scroll-container" flex="1" overflowY="auto">
+      <Box
+        id="scroll-container"
+        h="82vh"
+        overflowY="auto"
+        pt={isHome ? 0 : 4}
+      >
         <Outlet />
       </Box>
 
